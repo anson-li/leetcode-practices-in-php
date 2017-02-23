c16cccd1d3b1e9df742bb12a6a2c8221ffd94153
Eden Tsai <edentsai231@gmail.com>
2017-02-24 01:56:00 +0800

    MultipleSum should return array with answer when two sum solved

--- src/Exercise001/MultipleSum.php
+++ src/Exercise001/MultipleSum.php
@@ -18,7 +18,22 @@ class MultipleSum
      */
     public function solve(int $amount, int $target, array $numbers) : array
     {
-        if (1 === $amount) {
+        if ($amount > 1) {
+            $remain['amount'] = $amount - 1;
+            foreach ($numbers as $index => $number) {
+                $remain['target'] = $target - $number;
+                $remain['numbers'] = $numbers;
+                unset($remain['numbers'][$index]);
+                try {
+                    return array_merge(
+                        [$index],
+                        $this->solve($remain['amount'], $remain['target'], $remain['numbers'])
+                    );
+                } catch (InvalidArgumentException $exception) {
+                    continue;
+                }
+            }
+        } elseif (1 === $amount) {
             return $this->oneSum($target, $numbers);
         }
 
--- tests/Exercise001/MultipleSumTest.php
+++ tests/Exercise001/MultipleSumTest.php
@@ -66,4 +66,66 @@ class MultipleSumTest extends TestCase
 
         $this->assertSame($expected, $actual);
     }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testTwoSumShouldThrowInvalidArgumentExceptionWhenCannotBeSolved()
+    {
+        $amount = 2;
+        $target = 999;
+        $numbers = [2, 7, 11, 15];
+
+        $multipleSum = new MultipleSum();
+        $actual = $multipleSum->solve($amount, $target, $numbers);
+    }
+
+    public function twoSumDataProvider() : array
+    {
+        return [
+            [
+                'target' => 9,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [0, 1],
+            ],
+            [
+                'target' => 13,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [0, 2],
+            ],
+            [
+                'target' => 17,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [0, 3],
+            ],
+            [
+                'target' => 18,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [1, 2],
+            ],
+            [
+                'target' => 22,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [1, 3],
+            ],
+            [
+                'target' => 26,
+                'numbers' => [2, 7, 11, 15],
+                'expected' => [2, 3],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider twoSumDataProvider
+     */
+    public function testTwoSumShouldReturnArrayWithAnswerWhenSolved(int $target, array $numbers, array $expected)
+    {
+        $amount = 2;
+
+        $multipleSum = new MultipleSum();
+        $actual = $multipleSum->solve($amount, $target, $numbers);
+
+        $this->assertSame($expected, $actual);
+    }
 }
